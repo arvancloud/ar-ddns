206974665020f2eeb61eac59d6da8a4fde9936dc
pooria <pooria.ghaedi@gmail.com>
2021-01-12 09:38:05 +0330

    bug fixes

--- ddns.sh
+++ ddns.sh
@@ -5,12 +5,14 @@ domain_name="example.com"
 
 root_domain=$(echo $domain_name | awk -F\. '{print $(NF-1) FS $NF}' )
 
+###check if its a root domain ro subdomain
 if [ "$(echo $domain_name | awk -F"." '{print $3}')" == "" ] ; then
 	subdomain="@"
 else 
 	subdomain=$(echo $domain_name | awk -F"." '{print $1}')
 fi
 
+### get current IP From Arvancloud DNS
 data=$(curl -s -L -X GET \
    "https://napi.arvancloud.com/cdn/4.0/domains/$root_domain/dns-records" \
    -H "Content-Type: application/json" \
@@ -21,20 +23,22 @@ data=$(curl -s -L -X GET \
 current_IP=$(echo $data | jq '.data[] | select(.type=="a") |  select(.name=="'$subdomain'") ' | jq '.value[].ip' 2> /dev/null)
 domain_id=$(echo $data |  jq '.data[] | select(.type=="a")   |  select(.name=="'$subdomain'") ' | jq '.id' 2> /dev/null | sed 's/^"\(.*\)".*/\1/' )
 
-response=$(curl -s -o /dev/null -w ''%{http_code}'' https://wtfismyip.com/text);
-if [ "${response}" == "200" ]; then
+### get system's current IP
+response=$(curl -s -o /dev/null -w ''%{http_code}'' https://ipinfo.io/ip);
+if [ "${response}" != "200" ]; then
 	response=$(curl -s https://wtfismyip.com/text)
 else
 	response=$(curl -s  https://ipinfo.io/ip)
 fi
 
+### set System's current IP into Arvancloud DNS
 if  [ ${current_IP} == "${response}" ] ; then
 	echo "No action Needed"
 else
-	PutNewIP=$(curl -s -L -X PUT "https://napi.arvancloud.com/cdn/4.0/domains/$root_domain/dns-records/$domain_id" \
+	PutNewIP=$(curl -s -L -X PUT "https://napi.arvancloud.com/cdn/4.0/domains/vimoon.ir/dns-records/e65a8730-49d3-453e-9e94-8af37e634887" \
    -H "Content-Type: application/json" \
    -H "Accept: application/json" \
    -H "Authorization: ${TOKEN}" \
-   -d "{"name": "$subdomain", "type": "a", "value": "[{"ip": "$response"}]"}")
+   -d "{ \"name\": \"$subdomain\", \"type\": \"a\", \"value\": [{\"ip\": \"$response\"}]}")
 echo $PutNewIP
 fi
